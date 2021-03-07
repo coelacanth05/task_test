9b9c5f4d6b3d336bfddfc034c42007ccba85b9ba
kai <coelacanth2505@gmail.com>
2021-03-07 21:19:24 +0900

    107. destroy 削除機能

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -148,5 +148,8 @@ class ContactFormController extends Controller
     public function destroy($id)
     {
         //
+        $contact = ContactForm::find($id);
+        $contact->delete();
+        return redirect('contact/index');
     }
 }
